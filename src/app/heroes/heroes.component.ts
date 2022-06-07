fe2f547a55c7e217bc461fd2304dd8ea10743f98
Andrew Bradt <andrewjbradt@gmail.com>
2022-06-07 14:08:53 -0400

    make heroes render list of heroes and conditionally render hero details if a hero has been clicked

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,14 +8,19 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'windstorm'
-  };
+  selectedHero?: Hero;
+  heroes = HEROES;
 
   constructor() {}
 
   ngOnInit(): void {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  onClose(): void {
+    this.selectedHero = undefined;
+  }
 }
